15e1b1d9f125ddaef6be3dc0830302a9f0cf479f
Aaron E-J <the@otherrealm.org>
2018-08-30 17:44:47 -0400

    Release notes for 2.0.1

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ To look up a word on Wikipedia – Just highlight the word and click Ctrl+Shift+
 To look up a word on Wiktionary – Just highlight the word and click Ctrl+Shift+2 (Command+Shift+2 on mac) and an overlay of the Wiktionary article will appear as an in-page overlay.
 
 To close the overlay, click Ctrl+Shift+~ (Command+Shift+~ on mac) (the key to the left of the '1' key if you have a different keyboard that doesn't have the `/~ key)
+
+This code uses the following open source code:
+jquery-3.3.1
+jquery-ui-1.10.2
--- public_html/pageMod.js
+++ public_html/pageMod.js
@@ -14,69 +14,72 @@
 var selObj = '';
 selObj = window.getSelection();
 var ctrlDown = false;
+//var x = window.screenX;
+//var y = window.screenY;
+var nOfLookups = 1;
+var activePage='';
 $(document).keydown(function (e) {
-	if (e.ctrlKey&&e.shiftKey&&e.which ===49) {
+	if (e.ctrlKey && e.shiftKey && e.which === 49) {
 		getSelectedPedia('ctrl');
 	}
-	if (e.ctrlKey&&e.shiftKey&&e.which ===50) {
+	if (e.ctrlKey && e.shiftKey && e.which === 50) {
 		getSelectedTionary('alt');
 	}
-	if (e.ctrlKey&&e.shiftKey&&e.which ===192) {
-		removeSelected('shift');
+	if (e.ctrlKey && e.shiftKey && e.which === 192) {
+		closeWiki('shift');
 	}
-	
 });
+////This gets the window location
+//function whereIsTheScreen() {
+//    x = window.screenX;
+//    y = window.screenY;
+//};
 var getSelectedPedia = function (command) {
-	console.log(command);
 	selObj = window.getSelection();
-	$('body').prepend('<div class="wikiAddonDivRap" id="wikiAddonDivRap" style="position: absolute;  top:' + $('#hiddenY').html() + 'px;left:' + $('#hiddenX').html() + 'px"">' +
+	$('body').append('<div class="wikiAddonDivRap" id="wikiAddonDivRap' + nOfLookups++ + '" style="position: fixed;  top:' + (nOfLookups * 10) + 'px;left:' + (nOfLookups * 10) + 'px"">' +
 		'<div class="btnForTheAddon btn-large IconBtnForTheAddon" type="button" style="padding: 5px;font-family: Arial, Helvetica, sans-serif; font-size: 30px;" id="moveIconBtn"> + </div>' +
-		'<a href="javascript:" onclick="closeWiki()"><div type="button" class="btnForTheAddon btn-large IconBtnForTheAddon" style="padding: 5px; font-size: 25px;font-family: Arial, Helvetica, sans-serif;" id="removeIconBtn"> x </div></a>' +
+		'<a href="javascript:" onclick="closeWiki()"><div type="button" class="btnForTheAddon removeIconBtn btn-large IconBtnForTheAddon" style="padding: 5px; font-size: 25px;font-family: Arial, Helvetica, sans-serif;" id="removeIconBtn"> x </div></a>' +
 		'<iframe id="wikiFrameContent" style="" src="https://en.wikipedia.org/wiki/Special:Search/' + selObj + '"></iframe>' +
 		'</div>');
+	//console.log(nOfLookups,activePage);
 	$(function () {
-		$("#wikiAddonDivRap").draggable();
-		$("#wikiAddonDivRap").resizable();
-		$('#removeIconBtn').click(function () {
+		$(".wikiAddonDivRap").draggable();
+		$(".wikiAddonDivRap").resizable();
+		$('.removeIconBtn').click(function () {
+			nOfLookups--;
 			$(this).parent().parent().remove();
 		});
 	});
-}
+};
 var getSelectedTionary = function (command) {
-	console.log(command);
+	//console.log(command);
 	selObj = window.getSelection();
-	$('body').prepend('<div class="wikiAddonDivRap" id="wikiAddonDivRap" style="position: absolute;  top:' + $('#hiddenY').html() + 'px;left:' + $('#hiddenX').html() + 'px"">' +
+	$('body').append('<div class="wikiAddonDivRap" id="wikiAddonDivRap' + nOfLookups++ + '" style="position: fixed;  top:' + nOfLookups * 10 + 'px;left:' + nOfLookups * 10 + 'px"">' +
 		'<div class="btnForTheAddon btn-large IconBtnForTheAddon" type="button" style="padding: 5px;font-family: Arial, Helvetica, sans-serif; font-size: 30px;" id="moveIconBtn"> + </div>' +
-		'<a href="javascript:" onclick="closeWiki()"><div type="button" class="btnForTheAddon btn-large IconBtnForTheAddon" style="padding: 5px; font-size: 25px;font-family: Arial, Helvetica, sans-serif;" id="removeIconBtn"> x </div></a>' +
+		'<a href="javascript:" onclick="closeWiki()"><div type="button" class="btnForTheAddon removeIconBtn btn-large IconBtnForTheAddon" style="padding: 5px; font-size: 25px;font-family: Arial, Helvetica, sans-serif;" id="removeIconBtn"> x </div></a>' +
 		'<iframe id="wikiFrameContent" style="" src="https://en.wiktionary.org/wiki/Special:Search/' + selObj + '"></iframe>' +
 		'</div>');
+	//console.log(nOfLookups);
 	$(function () {
-		$('#wikiAddonDivRap').on('onload', 'ready', 'change', function () {
+		$('.wikiAddonDivRap').on('onload', 'ready', 'change', function () {
 			var wikiZIndex = $(this).css('z-index');
 			wikiZIndex++;
 			$(this).css('z-index', wikiZIndex);
-			console.log($(this).css('z-index'));
+			//console.log($(this).css('z-index'));
 		});
-		$("#wikiAddonDivRap").draggable();
-		$("#wikiAddonDivRap").resizable();
-		$('#removeIconBtn').click(function () {
+		$(".wikiAddonDivRap").draggable();
+		$(".wikiAddonDivRap").resizable();
+		$('.removeIconBtn').click(function () {
+			nOfLookups--;
 			$(this).parent().parent().remove();
 		});
 	});
 };
+//remove the iframe when the key combinations are pressed
 var removeSelected = function (command) {
-	console.log(command);
 	closeWiki();
 };
-//remove the iframe when the key combinations are pressed
-
-function closeWiki() {
-	$('#wikiAddonDivRap').remove();
-}
-console.log(selObj);
-//This gets the cursor location and sends it to main.js
-/* self.port.on('show', function() {
-    var x = window.screenX;
-    var y = window.screenY;
-    self.port.emit("windowSize", x, y);
-}); */
+var closeWiki=function () {
+	//console.log($('.wikiAddonDivRap').last());
+	$('.wikiAddonDivRap').last().remove();
+};
